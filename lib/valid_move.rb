85925faa25d2172fba36531fac15299a0c4cff5d
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-08 19:24:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if board[index] == "" and board[index] < 9
     return true
-  
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
